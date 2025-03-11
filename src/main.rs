da8857bf9572befd3727ed3473e3900be672b84c
Gabriel Nilsson <gabriel.nilsson@mindroad.se>
2025-03-11 14:22:29 +0100

    Unstable: try adding uart and try writing to screen in chunks

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 #![no_std]
 #![no_main]
 
+use core::cell::RefCell;
 use bsp::entry;
 use defmt::*;
 use defmt_rtt as _;
@@ -21,6 +22,11 @@ use bsp::hal::{
     sio::Sio,
     watchdog::Watchdog,
 };
+use nano_rust_drivers::{error, ssd1306, ssd1306_registers};
+use nano_rust_drivers::ssd1306_registers::{BLACK, WHITE};
+use rp2040_hal::fugit::RateExtU32;
+use rp2040_hal::I2C;
+use rp2040_hal::uart::{DataBits, StopBits, UartConfig, UartPeripheral};
 
 #[entry]
 fn main() -> ! {
@@ -52,6 +58,18 @@ fn main() -> ! {
         sio.gpio_bank0,
         &mut pac.RESETS,
     );
+    let uart_pins = (
+        pins.gpio0.into_function(),
+        pins.gpio1.into_function(),
+    );
+    let uart = UartPeripheral::new(pac.UART0, uart_pins, &mut pac.RESETS)
+        .enable(
+            UartConfig::new(115200.Hz(), DataBits::Eight, None, StopBits::One),
+            clocks.peripheral_clock.freq(),
+        ).unwrap();
+
+    uart.write_full_blocking(b"Hello World!\r\n");
+    // info!("Startup");
 
     // This is the correct pin on the Raspberry Pico board. On other boards, even if they have an
     // on-board LED, it might need to be changed.
@@ -64,6 +82,190 @@ fn main() -> ! {
     // in series with the LED.
     let mut led_pin = pins.led.into_push_pull_output();
 
+
+    // let dp = Peripherals::take().unwrap();
+    // let pins = arduino_hal::pins!(dp);
+    // let mut serial = arduino_hal::default_serial!(dp, pins, 115200);
+    // print::put_console(serial);
+
+    // let result = (|| -> Result<(), error::UDisplayError<arduino_hal::i2c::Error>> {
+    //     let mut i2c = arduino_hal::I2c::new(
+    //         dp.TWI,
+    //         pins.a4.into_pull_up_input(),
+    //         pins.a5.into_pull_up_input(),
+    //         50000,
+    //     );
+    //     let mut led = pins.d13.into_output();
+    //     let mut button1 = pins.d5.into_pull_up_input();
+    //     let mut button2 = pins.d4.into_pull_up_input();
+    //     let mut button1_last_pressed = button1.is_low();
+    //     let mut button2_last_pressed = button2.is_low();
+    //     let i2c_ref_cell = RefCell::new(i2c);
+    //     let mut accelerometer = bmi160::Driver::new(embedded_hal_bus::i2c::RefCellDevice::new(&i2c_ref_cell), None, None)?;
+    //     let mut buffer = [0x00; ssd1306::BUFFER_SIZE];
+    //     let display_result = ssd1306::DisplayDriver::new(embedded_hal_bus::i2c::RefCellDevice::new(&i2c_ref_cell), None, &mut buffer);
+    //     // print::print_type_name(&display_result);
+    //     //
+    //     // core::result::Result<
+    //     // hackathon_pong_controller::ssd1306::DisplayDriver<embedded_hal_bus::i2c::refcell::RefCellDevice<avr_hal_generic::i2c::I2c<atmega_hal::Atmega,
+    //     // avr_device::devices::atmega328p::TWI,
+    //     // avr_hal_generic::port::Pin<avr_hal_generic::port::mode::Input,
+    //     // atmega_hal::port::PC4>,
+    //     // avr_hal_generic::port::Pin<avr_hal_generic::port::mode::Input,
+    //     // atmega_hal::port::PC5>,
+    //     // avr_hal_generic::clock::MHz16>>>,
+    //     // hackathon_pong_controller::ssd1306_error::Error<avr_hal_generic::i2c::Error>>
+    //     let mut display = match display_result {
+    //         Ok(display) => {
+    //             display
+    //         }
+    //         Err(error) => {
+    //             return Err(error.into());
+    //         }
+    //     };
+    //
+    //     // println!("BMI160 initialized");
+    //     info!("Started");
+
+    // loop {
+    //     // let button1_pressed = button1.is_low();
+    //     // let button2_pressed = button2.is_low();
+    //     // if button1_pressed && !button1_last_pressed {
+    //     //     println!("Button1 pressed");
+    //     // }
+    //     // if button2_pressed && !button2_last_pressed {
+    //     //     println!("Button2 pressed");
+    //     // }
+    //     // button1_last_pressed = button1_pressed;
+    //     // button2_last_pressed = button2_pressed;
+    //     //
+    //     // led.toggle();
+    //     // accelerometer.update()?;
+    //     // if let Some(output_data) = accelerometer.get_output_data() {
+    //     // }
+    //     println!("On");
+    //     display.fill_screen(ssd1306_registers::WHITE);
+    //     display.display()?;
+    //     led.set_high();
+    //     arduino_hal::delay_ms(2000);
+    //
+    //     println!("Off");
+    //     display.fill_screen(ssd1306_registers::BLACK);
+    //     display.display()?;
+    //     led.set_low();
+    //     arduino_hal::delay_ms(2000);
+    // }
+    let result = (|| -> Result<(), error::UDisplayError<bsp::hal::i2c::Error>> {
+        let mut i2c = I2C::i2c0(
+            pac.I2C0,
+            pins.gpio20.reconfigure(), // sda
+            pins.gpio21.reconfigure(), // scl
+            50.kHz(),
+            &mut pac.RESETS,
+            125_000_000.Hz(),
+        );
+        let i2c_ref_cell = RefCell::new(i2c);
+        let mut display_buffer = [0x00; ssd1306::BUFFER_SIZE];
+        let display_result = ssd1306::DisplayDriver::new(embedded_hal_bus::i2c::RefCellDevice::new(&i2c_ref_cell), None, &mut display_buffer);
+        let mut display = match display_result {
+            Ok(display) => {
+                display
+            }
+            Err(error) => {
+                return Err(error.into());
+            }
+        };
+
+        loop {
+            // delay.delay_ms(1000);
+            // display.fill_screen(ssd1306_registers::BLACK);
+            // display.display()?;
+            // led_pin.set_high().unwrap();
+            // delay.delay_ms(1000);
+            //
+            // display.fill_screen(ssd1306_registers::WHITE);
+            // display.display()?;
+            // led_pin.set_low().unwrap();
+            // delay.delay_ms(1000);
+            display.fill_screen(ssd1306_registers::BLACK);
+            display.display()?;
+            delay.delay_ms(1000);
+            display.fill_screen(ssd1306_registers::WHITE);
+            led_pin.set_high().unwrap();
+            for i in 0..=ssd1306::BUFFER_SIZE {
+                led_pin.set_high().unwrap();
+                delay.delay_ms(20);
+                led_pin.set_low().unwrap();
+                delay.delay_ms(20);
+                display.display_num(i)?;
+            }
+            led_pin.set_low().unwrap();
+            delay.delay_ms(1000);
+            // display.display_num(10)?;
+            // delay.delay_ms(1000);
+
+            // display.fill_screen(BLACK);
+            // display.display()?;
+            // delay.delay_ms(1000);
+            // display.fill_screen(WHITE);
+            // display.display()?;
+            // delay.delay_ms(1000);
+            // display.fill_screen(ssd1306_registers::BLACK);
+            // for i in 0..32 {
+            //     display.draw_pixel(i, i, WHITE)?;
+            // }
+            // for i in 0..32 {
+            //     display.draw_pixel(31 + i, 31, WHITE)?;
+            // }
+            // for i in 0..32 {
+            //     display.draw_pixel(63 + i, 31 - i, WHITE)?;
+            // }
+            // for i in 0..32 {
+            //     display.draw_pixel(95 + i, 0, WHITE)?;
+            // }
+            // display.display()?;
+            // delay.delay_ms(1000);
+            // display.draw_line(4, 4, 57, 25, WHITE)?;
+            // display.display()?;
+            // delay.delay_ms(1000);
+            // display.draw_line(57, 4, 4, 25, WHITE)?;
+            // display.display()?;
+            // delay.delay_ms(1000);
+            // display.draw_line(97, 15, 44, 30, WHITE)?;
+            // display.display()?;
+            // delay.delay_ms(1000);
+            // display.draw_line(44, 15, 97, 30, WHITE)?;
+            // display.display()?;
+            // delay.delay_ms(1000);
+            // display.draw_fill_rect(4, 4, 57 - 4, 25 - 4, WHITE)?;
+            // display.display()?;
+            // // display.dim(true)?;
+            // delay.delay_ms(1000);
+            // // display.dim(false)?;
+            // delay.delay_ms(1000);
+            //
+            // display.clear_display();
+            // display.set_cursor(0, 0);
+            // // display.draw_string("You're a mean one,\nMister Grinch!");
+            // display.draw_string(" Abc");
+            // display.display()?;
+            // delay.delay_ms(2000);
+        }
+    })();
+    if let Err(error) = result {
+        info!("Error");
+        loop {
+            // uart.write_full_blocking(b"on!\n");
+            // info!("on!");
+            led_pin.set_high().unwrap();
+            delay.delay_ms(100);
+            // uart.write_full_blocking(b"off!\n");
+            // info!("off!");
+            led_pin.set_low().unwrap();
+            delay.delay_ms(100);
+        }
+        // info!("Error: {}", error);
+    }
     loop {
         info!("on!");
         led_pin.set_high().unwrap();
