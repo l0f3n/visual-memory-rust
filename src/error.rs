9fb89c9b1982c75f6a3e719806225f3532e1f894
Gabriel Nilsson <gabriel.nilsson@mindroad.se>
2025-03-18 11:32:04 +0100

    Run actual game code on pc

--- src/error.rs
+++ src/error.rs
@@ -1,12 +1,17 @@
+use std::convert::Infallible;
+use std::fmt::{Display, Formatter, Write};
 #[cfg(not(target_arch = "x86_64"))]
 use defmt::Format;
 
 #[cfg_attr(not(target_arch = "x86_64"), derive(Format))]
+#[cfg_attr(target_arch = "x86_64", derive(Debug))]
 pub enum Error {
     #[cfg(target_arch = "arm")]
     I2c(rp2040_hal::i2c::Error),
     #[cfg(not(target_arch = "x86_64"))]
     Display(display_interface::DisplayError),
+    #[cfg(target_arch = "x86_64")]
+    Infallible,
     Format,
 }
 #[cfg(not(target_arch = "x86_64"))]
@@ -28,4 +33,17 @@ impl From<core::fmt::Error> for Error {
         Self::Format
     }
 }
+impl From<Infallible> for Error {
+    fn from(_value: Infallible) -> Self {
+        Self::Infallible
+    }
+}
 
+// impl Display for Error {
+//     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+//         match self {
+//             Error::Infallible => f.write_str("Infallible"),
+//             Error::Format => f.write_str("Format"),
+//         }
+//     }
+// }
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,14 @@
 #![cfg_attr(not(target_arch = "x86_64"), no_main)]
 
 use core::panic::PanicInfo;
+use std::time::Duration;
 
 mod error;
 mod game;
 mod debouncing;
 mod main_rp2040;
 
-use embedded_graphics::Drawable;
+use embedded_graphics::{Drawable, Pixel};
 use embedded_graphics::mono_font::MonoTextStyle;
 use embedded_graphics::mono_font::ascii::*;
 use embedded_graphics::pixelcolor::BinaryColor;
@@ -24,11 +25,15 @@ use bsp::entry;
 use defmt::*;
 #[cfg(not(target_arch = "x86_64"))]
 use defmt_rtt as _;
+use embedded_graphics::draw_target::DrawTarget;
+use embedded_graphics::geometry::Dimensions;
 #[cfg(not(target_arch = "x86_64"))]
 use panic_probe as _;
 
 #[cfg(target_arch = "x86_64")]
 use embedded_graphics_simulator::{BinaryColorTheme, OutputSettingsBuilder, SimulatorDisplay, Window};
+use crate::error::Error;
+use crate::game::Game;
 
 #[cfg(target_arch = "arm")]
 #[entry]
@@ -44,31 +49,107 @@ fn main() -> ! {
 
 
 #[cfg(target_arch = "x86_64")]
-fn main() -> Result<(), core::convert::Infallible> {
+fn main() -> Result<(), Error> {
     let mut display = SimulatorDisplay::<BinaryColor>::new(Size::new(128, 32));
-    let line_style = PrimitiveStyle::with_stroke(BinaryColor::On, 1);
-    let text_style = MonoTextStyle::new(&FONT_6X10, BinaryColor::On);
-    Circle::new(Point::new(72, 8), 48)
-        .into_styled(line_style)
-        .draw(&mut display)?;
-
-    Line::new(Point::new(48, 16), Point::new(8, 16))
-        .into_styled(line_style)
-        .draw(&mut display)?;
-
-    Line::new(Point::new(48, 16), Point::new(64, 32))
-        .into_styled(line_style)
-        .draw(&mut display)?;
-
-    Rectangle::new(Point::new(79, 15), Size::new(34, 34))
-        .into_styled(line_style)
-        .draw(&mut display)?;
-
-    Text::new("Hello World!", Point::new(5, 5), text_style).draw(&mut display)?;
     let output_settings = OutputSettingsBuilder::new()
         .theme(BinaryColorTheme::OledBlue)
         .build();
-    Window::new("Hello World", &output_settings).show_static(&display);
+    let window = Window::new("Hello World", &output_settings);
+    let display_and_window = DisplayAndWindow {
+        display,
+        window,
+    };
+    let get_button_1 = || {
+        false
+    };
+    let get_button_2 = || {
+        false
+    };
+    let set_led = |value| {
+
+    };
+    let delay_ms = |ms| {
+        std::thread::sleep(Duration::from_millis(ms as u64));
+    };
+    let seed = 4;
+    let mut game = Game::new(get_button_1, get_button_2, set_led, delay_ms, display_and_window, seed)?;
+    game.run_game()?;
+
+
+    // Window::new("Hello World", &output_settings).show_static(&display);
+    // let line_style = PrimitiveStyle::with_stroke(BinaryColor::On, 1);
+    // let text_style = MonoTextStyle::new(&FONT_6X10, BinaryColor::On);
+    // Circle::new(Point::new(72, 8), 48)
+    //     .into_styled(line_style)
+    //     .draw(&mut display)?;
+    //
+    // Line::new(Point::new(48, 16), Point::new(8, 16))
+    //     .into_styled(line_style)
+    //     .draw(&mut display)?;
+    //
+    // Line::new(Point::new(48, 16), Point::new(64, 32))
+    //     .into_styled(line_style)
+    //     .draw(&mut display)?;
+    //
+    // Rectangle::new(Point::new(79, 15), Size::new(34, 34))
+    //     .into_styled(line_style)
+    //     .draw(&mut display)?;
+    //
+    // Text::new("Hello World!", Point::new(5, 5), text_style).draw(&mut display)?;
 
     Ok(())
+}
+
+struct DisplayAndWindow {
+    display: SimulatorDisplay<BinaryColor>,
+    window: Window,
+}
+
+impl DrawTarget for DisplayAndWindow {
+    type Color = BinaryColor;
+    type Error = <SimulatorDisplay<BinaryColor> as DrawTarget>::Error;
+
+    fn draw_iter<I>(&mut self, pixels: I) -> Result<(), Self::Error>
+    where
+        I: IntoIterator<Item=Pixel<Self::Color>>
+    {
+        self.display.draw_iter(pixels)
+    }
+
+    fn fill_contiguous<I>(&mut self, area: &Rectangle, colors: I) -> Result<(), Self::Error>
+    where
+        I: IntoIterator<Item=Self::Color>
+    {
+        self.display.fill_contiguous(area, colors)
+    }
+
+    fn fill_solid(&mut self, area: &Rectangle, color: Self::Color) -> Result<(), Self::Error> {
+        self.display.fill_solid(area, color)
+    }
+
+    fn clear(&mut self, color: Self::Color) -> Result<(), Self::Error> {
+        self.display.clear(color)
+    }
+}
+
+impl Dimensions for DisplayAndWindow {
+    fn bounding_box(&self) -> Rectangle {
+        self.display.bounding_box()
+    }
+}
+
+impl game::ValidDisplay for DisplayAndWindow {
+    fn flush(&mut self) -> Result<(), Error> {
+        self.window.update(&self.display);
+        Ok(())
+    }
+}
+impl crate::game::ValidDisplay for SimulatorDisplay<BinaryColor> {
+    fn flush(&mut self) -> Result<(), Error> {
+        let output_settings = OutputSettingsBuilder::new()
+            .theme(BinaryColorTheme::OledBlue)
+            .build();
+        Window::new("Hello World", &output_settings).update(&self);
+        Ok(())
+    }
 }
