ed8fca832c8fda022e840ac70936a367d6a2c5f6
Gabriel Nilsson <gabriel.nilsson@mindroad.se>
2025-03-18 17:21:27 +0100

    Shoehorn in arduino nano project

--- build.rs
+++ build.rs
@@ -16,20 +16,22 @@ use std::path::PathBuf;
 fn main() {
     // Put `memory.x` in our output directory and ensure it's
     // on the linker search path.
-    let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
-    File::create(out.join("memory.x"))
-        .unwrap()
-        .write_all(include_bytes!("memory.x"))
-        .unwrap();
-    println!("cargo:rustc-link-search={}", out.display());
+    if env::var("TARGET").unwrap() == "thumbv6m-none-eabi" {
+        let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
+        File::create(out.join("memory.x"))
+            .unwrap()
+            .write_all(include_bytes!("memory.x"))
+            .unwrap();
+        println!("cargo:rustc-link-search={}", out.display());
+        // By default, Cargo will re-run a build script whenever
+        // any file in the project changes. By specifying `memory.x`
+        // here, we ensure the build script is only re-run when
+        // `memory.x` is changed.
+        println!("cargo:rerun-if-changed=memory.x");
+    }
     
     
-    // By default, Cargo will re-run a build script whenever
-    // any file in the project changes. By specifying `memory.x`
-    // here, we ensure the build script is only re-run when
-    // `memory.x` is changed.
-    println!("cargo:rerun-if-changed=memory.x");
-    
+
     if env::var("TARGET").unwrap() == "x86_64-pc-windows-gnu" {
         let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
         println!("cargo:rustc-link-search={}", std::path::Path::new(&dir).join("../libsdl2/x86_64-pc-windows-gnu").to_str().unwrap());
